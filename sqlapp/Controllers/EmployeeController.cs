9f89b7d5507d936021b8addc9eeffa6bac4c5d21
dhana <dhana@LAPTOP-A2N232T6>
2022-05-21 16:57:05 -0400

    Add project files.

---
+++ sqlapp/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using sqlapp.Services;
+using sqlapp.Models;
+using Microsoft.Extensions.Configuration;
+
+namespace sqlapp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeService employeeservice;
+        private readonly IConfiguration configuration;
+        public EmployeeController(EmployeeService service,IConfiguration iconfig)
+        {
+            employeeservice = service;
+            configuration = iconfig;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Employee> employees = employeeservice.GetEmployee(configuration.GetConnectionString("SqlConn"));
+            return View(employees);
+        }
+    }
+}
---
+++ sqlapp/Services/EmployeeService.cs
@@ -0,0 +1,61 @@
+﻿using sqlapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sqlapp.Services
+{
+    public class EmployeeService
+    {
+        private static string db_source = "demodbsqlserverdjay.database.windows.net";
+        private static string user = "dhananjay.mishra";
+        private static string password = "Jan@2023";
+        private static string database = "demodb";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = user;
+            _builder.Password = password;
+            _builder.InitialCatalog = database;
+            return new SqlConnection(_builder.ConnectionString);
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<Employee> GetEmployee(string connectionString)
+        {
+            List<Employee> employees = new List<Employee>();
+            string cmd="Select * from Employee";
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            SqlCommand sqlcmd = new SqlCommand(cmd,conn);
+            using (SqlDataReader reader= sqlcmd.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    Employee empl = new Employee()
+                    {
+                        EmployeeId = Convert.ToInt32(reader["EmployeeID"]),
+                        EmployeeName = Convert.ToString(reader["EmployeeName"])
+                    };
+                    employees.Add(empl);
+
+                }
+            }
+            return employees;
+
+            
+        }
+    }
+}
